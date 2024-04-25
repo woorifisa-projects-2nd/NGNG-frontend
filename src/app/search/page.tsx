36613b99f31af44a07c5ba6d9939909af319253d
mnjh01 <119735814+mnjihwan@users.noreply.github.com>
2024-04-25 16:12:20 +0900

    search 임시

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,3 +1,27 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Search() {
-  return <div className="text-slate-200">search page</div>;
+
+  const [page, setPage] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
+
+  const test = () => {
+
+    const url = `http://localhost:8080/search/${keyword}/${page}`;
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      }
+    };
+
+    fetch(url, options);
+  }
+
+  return <div className="text-slate-200">
+    <button type="button" onClick={test}>123</button>
+  </div>;
 }
--- src/components/layouts/header/components/SearchInput.tsx
+++ src/components/layouts/header/components/SearchInput.tsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
 import SearchIcon from "../design/SVG/search.svg";
+
 export default function SearchInput() {
+
+  const [keyword, setKeyword] = useState("");
+
   return (
     <div className="flex items-center w-full md:w-[28%] p-5 pt-0 md:p-0">
       <div className="relative left-[15px] w-0 h-0 bottom-[9px]">
@@ -7,6 +12,8 @@ export default function SearchInput() {
       </div>
       <input
         className="bg-light-gray dark:bg-black rounded h-9 focus:outline-none pl-12 text-black dark:text-white min-w-44 w-full"
+        value={keyword}
+        onChange={e => setKeyword(e.target.value)}
         placeholder="무엇을 원하세요?"
       />
     </div>
